8e6fa5147df2269a2ddbe60d485760d6f984cdea
Jayesskay <jayeskdev@gmail.com>
2022-09-28 16:10:32 +0200

    Simplified drop_as

--- src/type_info.rs
+++ src/type_info.rs
@@ -11,9 +11,7 @@ impl TypeInfo {
     #[must_use]
     pub(crate) fn new<T: 'static>() -> Self {
         unsafe fn drop_as<T>(bytes: *mut u8) {
-            let mut x = std::mem::MaybeUninit::uninit();
-            std::ptr::copy_nonoverlapping(bytes.cast::<T>(), x.as_mut_ptr(), 1);
-            x.assume_init();
+            std::ptr::read::<T>(bytes.cast());
         }
 
         Self {
